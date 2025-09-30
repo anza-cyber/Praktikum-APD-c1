c05a2ef5cd8029f37cfcbe464c561ea698dad55f
anza-cyber <rafalia04khanza@gmail.com>
2025-09-30 20:18:20 +0800

    post test 3

--- 2509106104-RafaliaKhanzaTaufik-PT-3.py
+++ 2509106104-RafaliaKhanzaTaufik-PT-3.py
@@ -7,34 +7,34 @@ print("Masukkan NIM Anda")
 nim = input()
 if nama == nama1 and nim == nim1:
     print("Pilih Pembayaran UKT Anda")
-    print("1. Lunas (Sekali Bayar): Biaya administrasi 1%")
-    print("2. Cicilan 2x per Semester: Biaya administrasi 5%")
-    print("3. Cicilan 4x per Semester: Biaya administrasi 8%")
-    print("4. Cicilan 6x per Semester: Biaya administrasi 12%")
+    print("1. Lunas (Sekali Bayar): BiayaAdmin 1%")
+    print("2. Cicilan 2x per Semester: BiayaAdmin 5%")
+    print("3. Cicilan 4x per Semester: BiayaAdmin 8%")
+    print("4. Cicilan 6x per Semester: BiayaAdmin 12%")
     print("Pilih Metode Pembayaran Anda")
     pilihan = input()
     if pilihan == "1":
         biayaAdmin = 0.01
         totalBayar = uKT + uKT * biayaAdmin
-        print("Total Bayar : Rp" + str(totalBayar))
+        print(f"Total Bayar: Rp{totalBayar:,.0f}")
     else:
         if pilihan == "2":
             biayaAdmin = 0.05
             totalBayar = uKT + uKT * biayaAdmin
             cicilan = totalBayar / 2
-            print("Total Bayar : Rp" + str(totalBayar) + " Cicilanya :" + str(cicilan))
+            print(f"Total Bayar: Rp{totalBayar:,.0f} Cicilan per Semester: Rp{cicilan:,.0f}")
         else:
             if pilihan == "3":
                 biayaAdmin = 0.08
                 totalBayar = uKT + uKT * biayaAdmin
                 cicilan = totalBayar / 4
-                print("Total Bayar : Rp" + str(totalBayar) + " Cicilanya :" + str(cicilan))
+                print(f"Total Bayar: Rp{totalBayar:,.0f} Cicilan per Semester: Rp{cicilan:,.0f}")
             else:
                 if pilihan == "4":
                     biayaAdmin = 0.12
                     totalBayar = uKT + uKT * biayaAdmin
                     cicilan = totalBayar / 6
-                    print("Total Bayar : Rp" + str(totalBayar) + " Cicilanya :" + str(cicilan))
+                    print(f"Total Bayar: Rp{totalBayar:,.0f} Cicilan per Semester: Rp{cicilan:,.0f}")
     print("Silahkan Melakukan Pembayaran Anda")
 else:
     print("Login Gagal")
